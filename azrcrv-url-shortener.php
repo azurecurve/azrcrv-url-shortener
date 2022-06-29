a75e11f3b5925cccf3ebb5bb1078619b12dedb74
azurecurve <github@azurecurve.co.uk>
2022-06-29 17:47:54 +0100

    * Update readme files.  * Update language template.  * Fix bug with azurecurve menu.

--- azrcrv-url-shortener.php
+++ azrcrv-url-shortener.php
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------------
  * Plugin Name: URL Shortener
  * Description: Automatically shortens URls for new posts, of all standard and custom types, and all past posts when loaded for the first time after activation; custom post type allows external links to be shortened.
- * Version: 1.5.2
+ * Version: 1.5.3
  * Author: azurecurve
  * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/url-shortener/
@@ -14,7 +14,7 @@
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Full
  * text of the license is available at https://www.gnu.org/licenses/gpl-2.0.html.
- * ------------------------------------------------------------------------------
+ * -------------- ----------------------------------------------------------------
  */
 
 // Prevent direct access.
